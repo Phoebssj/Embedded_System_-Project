7ee4e5c60d93f50b1439941041a57f9245c36fc0
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2023-11-06 18:46:27 -0500

    Demo the ultrasonic sensor! :^)

--- src/src.ino
+++ src/src.ino
@@ -1,14 +1,24 @@
+#include <Arduino.h>
 #include <HardwareSerial.h>
 #include "config.h"
+#include "UltrasonicSensor.h"
 
 // Explicitly declare `Serial` to get nice editor suggestions.
 extern HardwareSerial Serial;
 
+UltrasonicSensor us{ us_trigger, us_echo };
+
 void setup() {
   Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  Serial.println("Hello, Arduino!");
-  delay(1000);
+  Serial.print(us.measure_distance());
+  Serial.println(" in");
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
