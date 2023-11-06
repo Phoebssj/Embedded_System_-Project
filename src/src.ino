860c49d0c593cd97d6c6cc5a95096afcddcbd1bb
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2023-11-06 13:37:14 -0500

    Add an initial src.ino file

---
+++ src/src.ino
@@ -0,0 +1,13 @@
+#include <HardwareSerial.h>
+
+// Explicitly declare `Serial` to get nice editor suggestions.
+extern HardwareSerial Serial;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Hello, Arduino!");
+  delay(1000);
+}
