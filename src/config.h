36bd2bec0e6dab372608ba9cb6cfaf92c47c16a8
Daniel Kareh <33532334+dkareh@users.noreply.github.com>
2023-11-11 13:00:23 -0500

    Fix motor pin definitions

--- src/config.h
+++ src/config.h
@@ -10,15 +10,15 @@ const byte us_trigger = A1; // Trigger (digital)
 
 // H-Bridge for DC Motor (left)
 // ----------------------------
-const byte left_motor_en  = 5; // Enable (PWM using OC0B, timer 0)
-const byte left_motor_bck = 2; // Input 1
-const byte left_motor_fwd = 4; // Input 2
+const byte left_motor_en  = 5; // Enable A (PWM using OC0B, timer 0)
+const byte left_motor_bck = 4; // Input 1
+const byte left_motor_fwd = 7; // Input 2
 
 // H-Bridge for DC Motor (right)
 // -----------------------------
-const byte right_motor_en  = 6; // Enable (PWM using OC0A, timer 0)
-const byte right_motor_bck = 7; // Input 1
-const byte right_motor_fwd = 8; // Input 2
+const byte right_motor_en  = 6; // Enable B (PWM using OC0A, timer 0)
+const byte right_motor_bck = 2; // Input 3
+const byte right_motor_fwd = 9; // Input 4
 
 // 1838 IR Remote Receiver
 // -----------------------
