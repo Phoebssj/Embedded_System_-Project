d7d7f6594f22b4b7fca55ee230e685323548b74c
Sheryll J <stjacquet0407@eagle.fgcu.edu>
2023-11-28 22:32:03 -0500

    Update irremote.h
    redefine the constants fron irremote-congis to the irremote.h
    updated the enums to whcih all numbers are listed
    created the one-time setup (irconfigs).
    created a read input function using the ircommand as it's datatype (readIrcommad) .

--- src/irremote.h
+++ src/irremote.h
@@ -1,6 +1,17 @@
+// irremote.h
+// Des: irremote setup
+// Audit 11/28/23
+// ------------------------------------------------------------
+
 //irremote header
-#include "irremote_config.h"
+//#include "irremote_config.h"
 #include <IRremote.h>
+#define IR_USE_AVR_TIMER2
+
+//#include <IRremote.h>
+
+// define glboal constants and types (structs/enums)
+const byte IR_RECEIVE_PIN = 4; // Would need to be change to 12 for the signal.
 
 #ifndef IRREMOTE_CONTROLS
 #define IRREMOTE_CONTROLS
@@ -13,22 +24,22 @@ class Irremote_commands{
         F_Stop = 2,
         goBck = 4,
         ppause = 5,
-      //  goFwd = 6,
-     //   down = 8,
-     //   vol_- = 9,
-      //  up = 10,
-      //  bnt_0 = 12,
-      //  eq = 13,
-      //  rept = 14,
-      //  bnt_1 = 16,
-     //   bnt_2 =17,
-     //   bnt_3 = 18,
-    //    bnt_4 = 20,
-    //    bnt_5 = 21,
-    //    bnt_6 = 22,
-    //    bnt_7 = 24,
-     //   bnt_8 = 25,
-    //    bnt_9 = 26
+        goFwd = 6,
+        down = 8,
+        vol_- = 9,
+        up = 10,
+        bnt_0 = 12,
+        eq = 13,
+        rept = 14,
+        bnt_1 = 16,
+        bnt_2 =17,
+        bnt_3 = 18,
+        bnt_4 = 20,
+        bnt_5 = 21,
+        bnt_6 = 22,
+        bnt_7 = 24,
+		    bnt_8 = 25,
+        bnt_9 = 26
   	};
 	//====================================================
 	//Constructors:
@@ -36,8 +47,11 @@ class Irremote_commands{
 	IRremote_commands();
 	//=======================================================
 	//Methods
-	//=======================================================
-	void printcontrol(unsigned int rslt);
+	//=======================================================	
+	void irconfig(){};
+	ircommand readIrcommand(){return rslt;}
+	void printcontrol(unsigned int r);
+	
 
 	
 	
